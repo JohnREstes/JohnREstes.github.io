dbfdf8663069a2662b702ce71ed8b65d8a3f7226
John R. Estes <John.R.Estes@icloud.com>
2023-06-03 13:42:42 -0500

    portfoli page updates

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ const modal = document.getElementById("myModal");
 const closeModal = document.getElementById("closeModal"); 
 const projectIframe = document.getElementById("projectIframe"); 
 const loader = document.getElementById("loaderDiv");
+const carouselImage = document.querySelector('[data-image]');
 const stickyNav = navbar.offsetTop;
-const linkObject = {
+const linkObjectOld = {
     todo: "https://github.com/JohnREstes/Working-Projects/blob/main/Practice_JS/12-Todo%20List/index.html",
     tip: "https://github.com/JohnREstes/Working-Projects/blob/main/Practice_JS/11-Tip%20Calculator/index.html",
     calc: "https://github.com/JohnREstes/Working-Projects/blob/main/Practice_JS/10-Calculator/index.html",
@@ -19,6 +20,19 @@ const linkObject = {
     weather: "http://dondeestasyolanda.com/weather/",
     '456': "https://456word.com/"
 };
+const linkObject = {
+    Todo: "https://github.com/JohnREstes/Working-Projects/blob/main/Practice_JS/12-Todo%20List/index.html",
+    Tip: "https://github.com/JohnREstes/Working-Projects/blob/main/Practice_JS/11-Tip%20Calculator/index.html",
+    Calc: "https://github.com/JohnREstes/Working-Projects/blob/main/Practice_JS/10-Calculator/index.html",
+    Shop: "https://github.com/JohnREstes/Working-Projects/blob/main/Practice_JS/13-Shopping_Cart/index.html",
+    hangman: "https://github.com/JohnREstes/Working-Projects/blob/main/Practice_JS/21-Hangman/index.html",
+    tictactoe: "https://github.com/JohnREstes/Working-Projects/blob/main/Practice_JS/22-Tic_Tac_Toe/index.html",
+    War: "https://github.com/JohnREstes/Working-Projects/blob/main/Practice_JS/24-War/index.html",
+    RPSLS: "https://github.com/JohnREstes/Working-Projects/blob/main/Practice_JS/25-RPSLS/index.html",
+    weather: "http://dondeestasyolanda.com/weather/",
+    '456': "https://456word.com/"
+};
+const images = ["./projects/456.png", "./projects/weather.png", "./projects/Todo.png", "./projects/Tip.png", "./projects/Calc.png", "./projects/Shop.png", "./projects/hangman.png", "./projects/tictactoe.png", "./projects/War.png", "./projects/RPSLS.png"];
 
 const observer = new IntersectionObserver(entries => {
     if(!(entries[0].isIntersecting)){
@@ -38,9 +52,9 @@ projects.forEach(project =>{
         setTimeout(()=>{
             loader.style.zIndex = -20;
         },1250)
-        if(e.target.id === '456'){
+        if(e.target.id == '456'){
             projectIframe.src = linkObject[e.target.id]
-        } else if(e.target.id === 'weather' ){
+        } else if(e.target.id == 'weather' ){
             projectIframe.src = linkObject[e.target.id]  
             projectIframe.scrolling = 'yes';          
         } else {
@@ -67,4 +81,35 @@ function removeLoader() {
     setTimeout(()=>{
         loader.style.zIndex = -20;
     },500)
+}
+const leftArrow = document.getElementById('arrow-left');
+const rightArrow = document.getElementById('arrow-right');
+
+leftArrow.addEventListener('click', ()=>{
+    changeIframe('left');
+})
+rightArrow.addEventListener('click', ()=>{
+    changeIframe('right');
+})
+var imgNum = 0;
+function changeIframe(dir){
+    let id;
+    if(dir === 'left'){
+        imgNum --
+        if(imgNum === -1) imgNum = images.length - 1;
+    }else if(dir === 'right'){
+        imgNum ++
+        if(imgNum === images.length) imgNum = 0;
+    }
+    carouselImage.src = images[imgNum];
+    id = extractId(images[imgNum]);
+    carouselImage.id = id;
+}
+function extractId(str){
+  // Get the index of the last occurrence of the dot character.
+  const dotIndex = str.lastIndexOf('.');
+  // Get the index of the last occurrence of the slash character before the dot character.
+  const slashIndex = str.lastIndexOf('/', dotIndex - 1);
+  // Return the substring of the string from the slash character to the dot character.
+  return str.substring(slashIndex + 1, dotIndex);
 }
